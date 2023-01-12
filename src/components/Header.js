5c8de3ba606ece8bac1cce06e58376baea4a4af5
blpraveen <blpraveen2004@gmail.com>
2023-01-12 20:37:49 +0530

    Banner Image Guruji

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,10 +18,13 @@ export default class Header extends Component {
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
-
+         <div className="banner-image">
+            <div className="banner-oval"></div>
+          </div>
          <div className="row banner">
+         
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
+               <h1 className="responsive-headline">{resumeData.name}.</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
                </h3>
                <hr/>
